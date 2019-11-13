9e010a7030c6963c0187d6683b754031aa1f7146
gnardinosaur <54404101+gnardinosaur@users.noreply.github.com>
2019-11-12 23:30:05 -0500

    Revert "buy stock first v"

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,16 +1,2 @@
 class TransactionsController < ApplicationController
-
-
-    def new
-        @transaction = Transaction.new
-    end
-
-    def create
-        @transaction= Transaction.create(transaction_params)
-        redirect_to transaction_path(@transaction.portfolio_id)
-    end
-private
-    def transaction_params
-        params.require(:stock).permit(:stock_id)
-    end
 end
