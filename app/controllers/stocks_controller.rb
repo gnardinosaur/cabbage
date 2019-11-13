fc28d0ca06501f2bbf3758430e56e4728271ee22
gnardinosaur <github email address>
2019-11-13 17:22:53 -0500

    stock purchase button working

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -2,7 +2,7 @@ class StocksController < ApplicationController
 
     def index	   
         @stocks = Stock.all_stocks
-        
+        @portfolio_id = params[:portfolio_id]       #gives index view portfolio.id of the portfolio show view it came from 
     end
 
 end
--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -4,15 +4,15 @@ class TransactionsController < ApplicationController
     end
 
     def create
-        @transaction= Transaction.create(transaction_params)
-        @logged_in_user.portfolios
-
+        @stock = Stock.create(price: params[:stock][:share_price], symbol: params[:stock][:symbol])
+        @stock_id = @stock.id
+        @transaction = Transaction.create(stock_id: @stock_id, num_of_shares: transaction_params[:num_of_shares], portfolio_id: transaction_params[:portfolio_id], share_price: transaction_params[:share_price])
 
         redirect_to portfolio_path(@transaction.portfolio_id)
     end
 
   private
     def transaction_params
-        params.require(:stock).permit(:stock_id, :share_price, :num_of_shares)
+        params.require(:stock).permit(:share_price, :num_of_shares, :portfolio_id)
     end
 end
